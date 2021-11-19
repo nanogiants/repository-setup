a5a1ef0062e3ab8317b0f94154c90b4670eab515
Jens Sproede <j.sproede@nanogiants.de>
2021-11-19 15:21:47 +0100

    feat: add dot templates and switch to dot template rendering
    also adds README.md and new dependencies

---
+++ README.md
@@ -0,0 +1,17 @@
+# NanoGiants Repository Setup
+
+This project helps setting up the necessary Open Source related markdown files according to our default CODE_OF_CONDUCT and CONTRIBUTING (guidelines). It also includes generation of LICENSE file, either with our default license (MIT) or any other you want.
+
+## Setup
+
+```bash
+$ npm install -g @nanogiants/repository-setup
+```
+
+Then run:
+
+```bash
+$ ng-repo-setup
+```
+
+and answer the questions in your console. After the required files have been generated, you may edit the TODO comments in the generated Code of Conduct and Contributing files.
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const fs = require('fs/promises');
+const path = require('path');
 const prompts = require('prompts');
 const { getLicense, findLicense } = require('license');
-const path = require('path');
-const covgen = require('covgen');
+const dot = require('dot');
+
+dot.templateSettings.strip = false;
+dot.log = false;
+const dots = dot.process({ path: path.join(__dirname, 'templates') });
+
+const packageJsonPath = path.join(process.cwd(), 'package.json');
+
+const FILE_MAPPING = {
+  License: 'LICENSE.md',
+  Contributing: 'CONTRIBUTING.md',
+  CodeOfConduct: 'CODE_OF_CONDUCT.md',
+};
 
 (async () => {
-  const answers = await prompts([
-    {
+  const answers = await prompts(
+    Object.entries(FILE_MAPPING).map(([licenseName, fileName]) => ({
       type: 'toggle',
-      name: 'generateLicense',
-      message: 'Generate LICENSE file?',
+      name: licenseName,
+      message: `Generate ${fileName} file?`,
       initial: 'on',
-    },
-    {
-      type: 'toggle',
-      name: 'generateContributing',
-      message: 'Generate CONTRIBUTING.md file?',
-      initial: 'on',
-    },
-    {
-      type: 'toggle',
-      name: 'generateCodeOfConduct',
-      message: 'Generate CODE_OF_CONDUCT.md file?',
-      initial: 'on',
-    },
-  ]);
+    })),
+  );
 
-  if (answers.generateLicense) {
+  if (answers.License) {
     const possibleLicenses = findLicense('');
 
     const licenseAnswer = await prompts({
@@ -45,16 +45,94 @@ const covgen = require('covgen');
     await fs.writeFile(path.join(process.cwd(), 'LICENSE'), licenseText);
   }
 
-  if (answers.generateContributing) {
-    await fs.copyFile(
-      path.join(__dirname, 'resources', 'CONTRIBUTING.md'),
-      path.join(process.cwd(), 'CONTRIBUTING.md'),
-    );
+  if (answers.Contributing || answers.CodeOfConduct) {
+    let name,
+      slug = '',
+      repoUrl,
+      docsUrl;
+
+    if (await fs.stat(packageJsonPath)) {
+      const {
+        name: packageSlug,
+        repository: packageRepository,
+        homepage: packageHomepage,
+      } = JSON.parse(await fs.readFile(packageJsonPath));
+
+      name = packageSlug;
+      slug = packageSlug;
+      docsUrl = packageHomepage;
+
+      const repositoryUrl = packageRepository?.url;
+      if (repositoryUrl) {
+        repoUrl = repositoryUrl.endsWith('/') ? repositoryUrl : `${repositoryUrl}/`;
+      }
+    } else {
+      console.log('package.json does not exist');
+    }
+
+    const packageAnswers = await prompts([
+      {
+        type: 'text',
+        name: 'name',
+        message: 'Name of the project',
+        initial: slug,
+      },
+      {
+        type: 'text',
+        name: 'slug',
+        message: 'Slug of the project',
+        initial: slug,
+      },
+      {
+        type: 'text',
+        name: 'repoUrl',
+        message: 'Repo URL of the project',
+        initial: repoUrl,
+      },
+      {
+        type: 'text',
+        name: 'docsUrl',
+        message: 'Docs URL of the project',
+        initial: docsUrl,
+      },
+    ]);
+
+    name = packageAnswers.name;
+    slug = packageAnswers.slug;
+    repoUrl = packageAnswers.repoUrl;
+    docsUrl = packageAnswers.docsUrl;
+
+    const specs = {
+      project: {
+        name,
+        slug,
+        repoUrl,
+        docsUrl,
+      },
+      contributing: {
+        generate: answers.generateContributing,
+        emailSensitiveBugs: 'developers@nanogiants.de',
+      },
+      codeOfConduct: {
+        generate: answers.generateCodeOfConduct,
+        enforcementEmail: 'developers@nanogiants.de',
+        enforcementGuidelines: true,
+      },
+    };
+
+    const filePathContributing = path.join(process.cwd(), 'CONTRIBUTING.md');
+    const markdownContributing = dots.contributing(specs);
+
+    const filePathCoC = path.join(process.cwd(), 'CODE_OF_CONDUCT.md');
+    const markdownCoC = dots.codeOfConduct(specs);
+
+    await fs.writeFile(filePathCoC, markdownCoC, 'utf-8');
+    await fs.writeFile(filePathContributing, markdownContributing, 'utf-8');
   }
 
-  if (answers.generateCodeOfConduct) {
-    await covgen('developers@nanogiants.de', path.join(process.cwd(), 'CODE_OF_CONDUCT.md'));
+  for (const [file, created] of Object.entries(answers)) {
+    if (created) {
+      console.log(`${FILE_MAPPING[file]} has been created.`);
+    }
   }
-
-  console.log('All files have been created');
 })();
